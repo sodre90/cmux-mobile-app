85d5bd450ec61c306f314c5dba5ef7cc84e27f9d
sodre90 <erdos.peter.bme@gmail.com>
2026-07-02 23:12:51 +0200

    android: use non-deprecated LocalLifecycleOwner from lifecycle-runtime-compose

--- android/app/src/main/java/com/sodre90/cmuxremote/ui/pairing/PairingScreen.kt
+++ android/app/src/main/java/com/sodre90/cmuxremote/ui/pairing/PairingScreen.kt
@@ -30,10 +30,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
