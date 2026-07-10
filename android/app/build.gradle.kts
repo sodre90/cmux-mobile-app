bb0f123c786aaac5ed0ecd037fbdc5c29a936c7c
sodre90 <erdos.peter.bme@gmail.com>
2026-07-10 13:20:27 +0200

    android: migrate kotlinOptions{} to compilerOptions{} DSL
    kotlinOptions is deprecated by the Kotlin Gradle plugin; move jvmTarget to
    the top-level kotlin { compilerOptions { } } extension. Mechanical DSL
    migration, no behavior change.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,5 @@
 import java.io.File
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
@@ -41,9 +42,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
         buildConfig = true
@@ -52,6 +50,12 @@ android {
         resources {
             excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
